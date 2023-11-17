950b042618c16cc484097127c695d46389e6a7d7
Thalisson Almeida <134632200+almeidatl@users.noreply.github.com>
2023-11-17 18:38:37 -0300

    vite project created

---
+++ projeto-rey/src/components/Header.jsx
@@ -0,0 +1,22 @@
+export default function Header(){
+    return(
+        <>
+            <div className="logo">
+                <img
+                src="https://static.wixstatic.com/media/900907_619c88cbc2164d95aa9122b91f37971f~mv2.png/v1/fill/w_188,h_93,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Rey%20das%20Cadeiras%20-%20Marca%20Mais%20pequena_pn.png"
+                alt=""
+                />
+            </div>
+            
+            <ul>  
+                <li className="social">
+                
+                    <a href="https://api.whatsapp.com/send?phone=558391627839">
+                    <img src="images/whatsapp-logo.png" alt="" />
+                    </a>
+                
+                </li>
+            </ul>
+        </>
+    )
+}
