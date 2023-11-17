92c82b62ae1df04638eb553de85a2c165572f697
Thalisson Almeida <134632200+almeidatl@users.noreply.github.com>
2023-11-17 19:35:27 -0300

    creative stuck

--- projeto-rey/src/components/Header.jsx
+++ projeto-rey/src/components/Header.jsx
@@ -1,22 +1,23 @@
+import './Header.css'
+
 export default function Header(){
     return(
         <>
-            <div className="logo">
-                <img
-                src="https://static.wixstatic.com/media/900907_619c88cbc2164d95aa9122b91f37971f~mv2.png/v1/fill/w_188,h_93,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Rey%20das%20Cadeiras%20-%20Marca%20Mais%20pequena_pn.png"
-                alt=""
-                />
-            </div>
-            
-            <ul>  
-                <li className="social">
-                
+            <header>
+                <div className="headerContainer">
+                    <div className="logo">
+                        <img
+                        src="https://static.wixstatic.com/media/900907_619c88cbc2164d95aa9122b91f37971f~mv2.png/v1/fill/w_188,h_93,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Rey%20das%20Cadeiras%20-%20Marca%20Mais%20pequena_pn.png"
+                        alt=""
+                        />
+                    </div>
+                    
+                    
                     <a href="https://api.whatsapp.com/send?phone=558391627839">
-                    <img src="images/whatsapp-logo.png" alt="" />
+                        <img className='social-image' src="../src/assets/images/whatsapp-logo.png" alt="zap-logo" />
                     </a>
-                
-                </li>
-            </ul>
+                </div>
+            </header>
         </>
     )
 }
